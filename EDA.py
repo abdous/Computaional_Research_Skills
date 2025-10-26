f3ae9ff6742cb55b54a6e73d7a9dd34ad2e5b519
Dourhamane Abdou Sanda <abdous2004@yahoo.fr>
2025-10-26 17:58:21 +0100

    separate the df with respect to the patient type.

--- EDA.py
+++ EDA.py
@@ -6,10 +6,10 @@ from datetime import datetime, timedelta
 
 # Make some constants for easy adjustment
 DATA_PATH = r"C:\Users\dourh\OneDrive\Bureau\UM\Computational Research Skills(E&OR)\ScanRecords.csv"  # <-- replace with your dataset filename
-DATE_COL = "Date"
-TIME_COL = "Time"
-DUR_COL = "Duration"
-TYPE_COL = "PatientType"
+# DATE_COL = "Date"
+# TIME_COL = "Time"
+# DUR_COL = "Duration"
+# TYPE_COL = "PatientType"
 
 # Data loading and initial exploration
 df = pd.read_csv(DATA_PATH)
@@ -22,17 +22,36 @@ print("Rows,cols:", df.shape)
 def make_datetime(row):
     # Make sure to change time fraction format from 9.5 to 9:30
     try:
-        t = float(row[TIME_COL])
+        t = float(row["Time"])
     except:
         # if time is already a string like "09:30" adjust accordingly
         try:
-            return pd.to_datetime(row[DATE_COL] + " " + row[TIME_COL])
+            return pd.to_datetime(row["Date"] + " " + row["Time"])
         except:
             return pd.NaT
     hours = int(np.floor(t))
     minutes = int(round((t - hours) * 60))
-    return pd.to_datetime(row[DATE_COL]) + pd.Timedelta(hours=hours, minutes=minutes)
+    return pd.to_datetime(row["Date"]) + pd.Timedelta(hours=hours, minutes=minutes)
 
 
 df["Datetime"] = df.apply(make_datetime, axis=1)
-print(df[["Date", TIME_COL, "Datetime"]].head())
+# print(df[["Date", TIME_COL, "Datetime"]].head())
+
+
+# Check for missing or invalid datetimes
+# print("\nChecking datetime values:")
+# print(f"Total rows: {len(df)}")
+# print(f"Missing datetime values: {df['Datetime'].isna().sum()}")
+# print(f"Percentage missing: {(df['Datetime'].isna().sum() / len(df)) * 100:.2f}%")
+# print(f"Missing Duration values: {df['Duration'].isna().sum()}")
+# print(f"Percentage missing: {(df['Duration'].isna().sum() / len(df)) * 100:.2f}%")
+# i check for invalid datetime/duration entries and there are none in this dataset
+
+#  Separate dataframes by patient type in dictionary dfs for easy access later
+#  print(f"Missing datetime values: {df['PatientType'].isna().sum()}") # there are no missing patient type entries
+patientType_values = df["PatientType"].unique()
+print("Patient types:", patientType_values)
+splitDFs = {tp: df[df["PatientType"] == tp].copy() for tp in patientType_values}
+print("splitDFs", splitDFs)
+
+# print(df.head())
