b140b870eccb771efb4233e6533a5f9e00d576e3
Dourhamane Abdou Sanda <abdous2004@yahoo.fr>
2025-10-26 17:05:07 +0100

    parse date time to new call Datetime.

--- EDA.py
+++ EDA.py
@@ -14,5 +14,25 @@ TYPE_COL = "PatientType"
 # Data loading and initial exploration
 df = pd.read_csv(DATA_PATH)
 print("Rows,cols:", df.shape)
-print(df.head())
-print(df.info())
+# print(df.head())
+# print(df.info())
+
+
+# Parse date and time into single column Datetime
+def make_datetime(row):
+    # Make sure to change time fraction format from 9.5 to 9:30
+    try:
+        t = float(row[TIME_COL])
+    except:
+        # if time is already a string like "09:30" adjust accordingly
+        try:
+            return pd.to_datetime(row[DATE_COL] + " " + row[TIME_COL])
+        except:
+            return pd.NaT
+    hours = int(np.floor(t))
+    minutes = int(round((t - hours) * 60))
+    return pd.to_datetime(row[DATE_COL]) + pd.Timedelta(hours=hours, minutes=minutes)
+
+
+df["Datetime"] = df.apply(make_datetime, axis=1)
+print(df[["Date", TIME_COL, "Datetime"]].head())
